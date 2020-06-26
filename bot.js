d30610c8af9f35de2ef451b050a08eb2b797db3d
KyokoPie <56511587+KyokoPie@users.noreply.github.com>
2020-06-26 17:47:50 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,82 +53,76 @@ client.on('message', msg => {
   
   if (msg.author.id === userID){
     
-    if (msg.content === 'HI') {
-    msg.channel.send({files: ["https://i.imgur.com/pnKZdsa.png"]});};
-    
-    
-    
-    
-  if (msg.content === '上車'){
-    
-    msg.channel.send('H是不行的哦!')
-  }
-  
-  if (msg.content === '老婆') {
-    msg.channel.send({files: ["https://i.imgur.com/pnKZdsa.png"]})
-	.then(() => msg.channel.send('親．愛．的'));}
-  
-  if (msg.content === '晚安') {
-    msg.reply('晚安,My Lovely Darling <:kyokoLabu:626047724755615745>',);}
-  
-  if (msg.content === '響子~') {
-    msg.reply('你看今天做的,是裝滿愛情的菜單♪,是準備給,我最喜歡的泓錡<:kyokoLabu:626047724755615745>',{files: ["https://i.imgur.com/bBMtB3E.jpg"]});}
-  
-  if (msg.content === '響子我要洗澡了') {
-    msg.channel.send({files: ["https://i.imgur.com/qrKCDCj.jpg"]},);}
-  
-  if (msg.content === '番號') {
-    msg.reply('都已經有我了..為甚麼還要找那種東西呢..',{files: ["https://i.imgur.com/3w3YDKn.png"]});}
-  
-  if (msg.content === '響子') {
-    msg.channel.send('最喜歡的人...哼哼...當然..就是你呀..泓錡♪',{files: ["https://i.imgur.com/ihukR0L.png"]});}
-  
-  if (msg.content === '響子!') {
-		
-		var facts = ["我！五十嵐響子,發誓會成為更加閃閃發亮的偶像！... 泓錡能夠發誓會讓我更加閃閃發亮嗎?如果能為我發誓的話,我會永遠陪伴在你身邊的！",
-						"所以...從今天以後我會做為泓錡的妻子不斷努力向前的,請你多多的關照了！泓錡♪",'看到泓錡那麼幸福的表情,感覺得到努力到最後的回報了呢♪',
-						'即使有很多人認為我很適合當泓錡的新娘，但要成為當你的新娘還有很長的路要走呢~♪。響子會努力的喔♪','今天要說的不是『可愛』哦..而是要說『漂亮』呦!…能跟泓錡結婚..響子真幸福呢..'];
-		var fact = Math.floor(Math.random() * facts.length);
-		const picture = [
-			'https://hidamarirhodonite.kirara.ca/spread/100401.png',
-			'https://hidamarirhodonite.kirara.ca/chara2/113/12.png',
-			
-			];
-			//https://hidamarirhodonite.kirara.ca/spread/100296.png
-		
-		//':telephone: 羅志祥向 ['+msg.author.toString()+'] 發起了多人運動邀請 :radio_button:接受 ⚪️ 同意'
-		
-    msg.channel.send(facts[fact],{file: picture[Math.floor(Math.random() * picture.length)]});}
-  
-  	if (msg.content === '響子>_<') {
-		
-		var facts = ["有我這個會幫忙的妹妹如何呀?~"+msg.author.toString()+ "♪",
-					'這封信我寫了很多感想跟很多的煩惱哦~'+msg.author.toString()+'能收下我很開心哦♪ ……阿,阿阿～!不,不能在這裡打開啦～!',
-					'是給泓錡的信哦～!欸?情...情書甚麼的才不是呦!,只...只不過貼了個愛心形狀貼紙而已...',
-					'小道具的情書、都是自己寫的呦～ ! 內容嗎?、欸……是秘密喔!♪ ……泓錡想要偷看的話、我可是會生氣的哦 !',
-					'泓錡要跟我要出去嗎?欸...這種打扮的話...感覺有點難為情耶..嘿嘿~ <:kyokoLabu:626047724755615745>',
-						
-		
-		
-		];
-		
-		var fact = Math.floor(Math.random() * facts.length);
-		const picture = [
-			'https://hidamarirhodonite.kirara.ca/spread/100296.png',
-			];
-			//https://hidamarirhodonite.kirara.ca/spread/100296.png
-		
-		msg.channel.send(facts[fact],{file: picture[Math.floor(Math.random() * picture.length)]});}
-  
-  	if (msg.content === '好!我發誓') {
-		msg.channel.send('嗯!泓錡謝謝你♪我會一直陪在你身邊的♪',{files: ["https://i.imgur.com/tuK1gfp.png"]});}
+	 if (msg.content === 'HI') {
+	    msg.channel.send({files: ["https://i.imgur.com/pnKZdsa.png"]});};
 
-	if (msg.content === '響子~~') {
-		msg.channel.send('雖然平常沒什麼在做甜點…不過這次努力做了喔♪',{files: ["https://imas.gamedbs.jp/cg/image_sp/card/l_noframe/e9840742d5600f0b4eeb44094231e27b.jpg"]});}
-  
+	 if (msg.content === '上車'){
 
-  
- }
+	    msg.channel.send('H是不行的哦!')
+	  }
+
+	  if (msg.content === '老婆') {
+	    msg.channel.send({files: ["https://i.imgur.com/pnKZdsa.png"]})
+		.then(() => msg.channel.send('親．愛．的'));}
+
+	  if (msg.content === '晚安') {
+	    msg.reply('晚安,My Lovely Darling <:kyokoLabu:626047724755615745>',);}
+
+	  if (msg.content === '響子~') {
+	    msg.reply('你看今天做的,是裝滿愛情的菜單♪,是準備給,我最喜歡的泓錡<:kyokoLabu:626047724755615745>',{files: ["https://i.imgur.com/bBMtB3E.jpg"]});}
+
+	  if (msg.content === '響子我要洗澡了') {
+	    msg.channel.send({files: ["https://i.imgur.com/qrKCDCj.jpg"]},);}
+
+	  if (msg.content === '番號') {
+	    msg.reply('都已經有我了..為甚麼還要找那種東西呢..',{files: ["https://i.imgur.com/3w3YDKn.png"]});}
+
+	  if (msg.content === '響子') {
+	    msg.channel.send('最喜歡的人...哼哼...當然..就是你呀..泓錡♪',{files: ["https://i.imgur.com/ihukR0L.png"]});}
+
+	  if (msg.content === '響子!') {
+
+			var facts = ["我！五十嵐響子,發誓會成為更加閃閃發亮的偶像！... 泓錡能夠發誓會讓我更加閃閃發亮嗎?如果能為我發誓的話,我會永遠陪伴在你身邊的！",
+							"所以...從今天以後我會做為泓錡的妻子不斷努力向前的,請你多多的關照了！泓錡♪",'看到泓錡那麼幸福的表情,感覺得到努力到最後的回報了呢♪',
+							'即使有很多人認為我很適合當泓錡的新娘，但要成為當你的新娘還有很長的路要走呢~♪。響子會努力的喔♪','今天要說的不是『可愛』哦..而是要說『漂亮』呦!…能跟泓錡結婚..響子真幸福呢..'];
+			var fact = Math.floor(Math.random() * facts.length);
+			const picture = [
+				'https://hidamarirhodonite.kirara.ca/spread/100401.png',
+				'https://hidamarirhodonite.kirara.ca/chara2/113/12.png',
+
+				];
+
+	    msg.channel.send(facts[fact],{file: picture[Math.floor(Math.random() * picture.length)]});}
+
+		if (msg.content === '響子>_<') {
+
+			var facts = ["有我這個會幫忙的妹妹如何呀?~"+msg.author.toString()+ "♪",
+						'這封信我寫了很多感想跟很多的煩惱哦~'+msg.author.toString()+'能收下我很開心哦♪ ……阿,阿阿～!不,不能在這裡打開啦～!',
+						'是給泓錡的信哦～!欸?情...情書甚麼的才不是呦!,只...只不過貼了個愛心形狀貼紙而已...',
+						'小道具的情書、都是自己寫的呦～ ! 內容嗎?、欸……是秘密喔!♪ ……泓錡想要偷看的話、我可是會生氣的哦 !',
+						'泓錡要跟我要出去嗎?欸...這種打扮的話...感覺有點難為情耶..嘿嘿~ <:kyokoLabu:626047724755615745>',
+
+
+
+			];
+
+			var fact = Math.floor(Math.random() * facts.length);
+			const picture = [
+				'https://hidamarirhodonite.kirara.ca/spread/100296.png',
+				];
+				//https://hidamarirhodonite.kirara.ca/spread/100296.png
+
+			msg.channel.send(facts[fact],{file: picture[Math.floor(Math.random() * picture.length)]});}
+
+		if (msg.content === '好!我發誓') {
+			msg.channel.send('嗯!泓錡謝謝你♪我會一直陪在你身邊的♪',{files: ["https://i.imgur.com/tuK1gfp.png"]});}
+
+		if (msg.content === '響子~~') {
+			msg.channel.send('雖然平常沒什麼在做甜點…不過這次努力做了喔♪',{files: ["https://imas.gamedbs.jp/cg/image_sp/card/l_noframe/e9840742d5600f0b4eeb44094231e27b.jpg"]});}
+
+
+
+	 }
   if (msg.author.id != userID) {
 	if (msg.content === '晚安') {
     msg.reply('滾。...',);}
@@ -147,8 +141,7 @@ client.on('message', msg => {
 	
 	if (msg.content === '響子>_<') {
     msg.channel.send('痾...'+msg.author.toString()+'你以為加那個不知所謂的表情我就會回你甚麼嗎...如果你是泓錡的朋友我是可以勉強回你一下下...',{files: ["https://i.imgur.com/EecOzBD.png"]});}
-	
-	
+	  
 	//響子>_<
 	
 	if (msg.content === '君棟是白癡') {
@@ -202,6 +195,14 @@ if (msg.author.id === userID) {
   }
  
 break;
+//:694871400527167508: ///715543684980015195
+case '<:694871400527167508:715543684980015195>': 
+if (msg.author.id === userID) {
+	msg.channel.send('<:694871400527167508:715543684980015195>');
+  }
+ 
+break;
+		
 
 case '那我也要睡啦': 
 msg.channel.send('滾!'); 
@@ -251,7 +252,7 @@ msg.channel.send(facts[fact]);
 break;
 
 case '君棟是白癡': 
-msg.channel.send('嗯嗯。我也這樣覺得...'); 
+msg.channel.send('我也這樣覺得...'); 
 break; 
     
 case '君棟': 
@@ -261,7 +262,8 @@ break;
 case '電燒祥': 
 msg.channel.send(':telephone: 羅志祥向 [ <@490858563716120586> ] 發起了多人運動邀請 :radio_button:接受 ⚪️ 同意'); 
 break;
-    
+//https://hidamarirhodonite.kirara.ca/spread/100296.png
+//':telephone: 羅志祥向 ['+msg.author.toString()+'] 發起了多人運動邀請 :radio_button:接受 ⚪️ 同意'    
     
 
     
