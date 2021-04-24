73c198f8f3145d6a57d1f0c261254dc22cee12e2
KyokoPie <56511587+KyokoPie@users.noreply.github.com>
2021-04-24 18:27:22 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Canvas = require('canvas');
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	// Since the image takes time to load, you should await it
+	const background = await Canvas.loadImage('./test.jpg');
+	// This uses the canvas dimensions to stretch the image onto the entire canvas
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+	// Use helpful Attachment class structure to process the file for you
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
+});
 client.on('message', message => {
     if (message.content === 'ping') {
         message.reply('pong');
