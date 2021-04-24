9b78b9f4176fe710902ecc780657da924f428396
KyokoPie <56511587+KyokoPie@users.noreply.github.com>
2021-04-25 03:46:14 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('guildMemberAdd', async (member) => {
     // Destructure the guild property from the member object
     const { guild } = member
     // Access the channel ID for this guild from the cache
-    const channelId = getChannelId(guild.id)
+    const channelId = 585563158974824448
     // Access the actual channel and send the message
     const channel = guild.channels.cache.get(channelId)
     // Create a canvas and access the 2d context
