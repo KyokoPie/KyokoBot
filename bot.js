e8159de068a051eaee783896351e14d82c23930e
KyokoPie <56511587+KyokoPie@users.noreply.github.com>
2021-04-27 14:17:26 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -445,7 +445,7 @@ client.on('message', message => {
   
    if (isReady && message.content === '君棟HI')
   {
-  DonHi = ++;
+  DonHi = DonHi++;
   isReady = false;
   var voiceChannel = message.member.voice.channel;
   voiceChannel.join().then(connection =>
