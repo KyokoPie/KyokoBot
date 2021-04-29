5904c87b82fa9520b3cacc0185d185e72786632d
KyokoPie <56511587+KyokoPie@users.noreply.github.com>
2021-04-29 23:30:57 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -216,7 +216,7 @@ client.on('message', msg => {
                         //[Math.floor(Math.random() * picture.length)]
                         //facts[fact],
                         msg.channel.send( {
-                                file: picture[Math.floor(Math.random() * picture.length)]
+                                file: [picture[Math.floor(Math.random() * picture.length)]]
                         });
                 }
 
