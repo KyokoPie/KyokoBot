51da5e99fd58f14eb26223ea7a59f653cb531049
KyokoPie <56511587+KyokoPie@users.noreply.github.com>
2021-06-05 22:55:21 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -591,8 +591,8 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
     
   const channelId = "585563158974824448"   
   
-  let newUserChannel = newMember.voice.channel
-  let oldUserChannel = oldMember.voice.channel
+  let newUserChannel = newMember.voiceChannel
+  let oldUserChannel = oldMember.voiceChannel
 
 
   if(oldUserChannel === undefined && newUserChannel !== undefined) {
