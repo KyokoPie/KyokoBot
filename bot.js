2894948bb4577806934b1bffcdc20026ec6f9dfc
KyokoPie <56511587+KyokoPie@users.noreply.github.com>
2021-06-05 22:18:54 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -590,7 +590,7 @@ client.on('message', message => {
 client.on('voiceStateUpdate', (oldMember, newMember) => {
   const channelId = "585563158974824448"
   const channel = guild.channels.cache.get(channelId)    
-  
+  const {guild} = member
   let newUserChannel = newMember.voiceChannel
   let oldUserChannel = oldMember.voiceChannel
 
